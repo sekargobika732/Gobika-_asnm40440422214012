a35e8bb0a49d5a5d6fba0a57699081a3bf1bdf6a
sekargobika732 <144471188+sekargobika732@users.noreply.github.com>
2023-10-16 12:47:04 +0530

    Add files via upload

---
+++ challange 3.2.py
@@ -0,0 +1,24 @@
+def sort_students(student_list):
+    # Sort the student list based on CGPA in descending order
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
+
+# Define a Student class
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+# Test the function with a list of student objects
+students = [
+    Student("Alice", "A101", 3.8),
+    Student("Bob", "B102", 3.9),
+    Student("Charlie", "C103", 3.7),
+]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
---
+++ challenge 3.1.py
@@ -0,0 +1,6 @@
+def linear_search_product(product_list, target_product):
+    indices = []
+    for index, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(index)
+    return indices
